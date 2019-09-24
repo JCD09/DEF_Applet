d217027bfc278f642221e7715f3621660fd360f4
JCD09 <mishachavarha@gmail.com>
2019-09-23 21:05:35 -0700

    hello world

--- default.nix
+++ default.nix
@@ -1,5 +1,9 @@
 with (import <nixpkgs> {});
+let
+  unstable = import <unstable> {};
+  nodejs_12 = callPackage ./packages/node_12_x.nix {};
+in
 stdenv.mkDerivation {
     name = "Environment";
-    nativeBuildInputs = with nodePackages; [ npm ];
+    nativeBuildInputs = with unstable.nodePackages; [ npm nodejs-11_x parcel-bundler];
 }
